efb59fdd26873ba033cbc41b3ba0853a1584eecb
islomsafarov929-cyber <islomsafarov929@gmail.com>
2025-11-04 19:00:20 +0300

    Create pythonhw1.py

---
+++ lesson-1/homework/pythonhw1.py
@@ -0,0 +1,54 @@
+# Task 1.With sides of squere, finding its perimeter and area.
+# To'rtburchak tomonlari bilan, uning perimetri va yuzasini topish.
+
+Width = 5
+Length = 8
+
+##Perimeter = (Width+Length) * 2 = 26
+##Area = Width*Length = 40
+
+print("Perimeter =", (Width+Length)*2)
+print("Area =", Width*Length)
+
+# Task 2.With diameter of circle, finding its length.
+# Aylana diametri bilan uning uzunnligini topish.
+
+diameter = 12
+
+##Length of circle = 2 * π * radius  = 12π
+##radius = diameter / 2 = 6
+
+radius = diameter/2
+
+print('Length of circle = ', 2 * radius, 'π')
+
+# Task 3.With two numbers a and b, finding their mean.
+# A va b sonlarining o'rtachasini topish.
+
+a = 9
+b = 4
+
+##Mean = (a + b)/2
+
+print('Mean = ', (a + b) / 2)
+
+# Task 4.With two numbers a and b, finding their sum, product and square of each number.
+# A va b sonlarining yig'indisi, ko'paytmasi va har birining kvadratini topish.
+
+a = 7 
+b = 4
+
+##Sum = a + b
+##Product = a * b
+##Square = a**2, b**2
+
+print('Sum =',a + b )
+print('Product =', a * b )
+print('Square of a =', a ** 2)
+print('Square of b =', b ** 2)
+
+
+
+
+
+
